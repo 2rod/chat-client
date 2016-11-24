e7d23e42a2dc02dd00bb8b198048e7c864a28586
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2016-11-24 19:09:13 +0100

    chat client v1.0

---
+++ src/main.js
@@ -0,0 +1,24 @@
+function parseQuote (response) {
+  $('.chatWindow').append('<div class="response">'+ response.quoteText + '</div>');
+}
+
+function getQuote() {
+  $.ajax({
+
+    url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote',
+    type: 'GET',
+    dataType: 'jsonp'
+  });
+}
+
+$(document).ready(function() {
+  $('#target').submit(function(event) {
+    event.preventDefault();
+    getQuote();
+  });
+
+
+
+
+});
+
