400a0451666c26fd636691247ad03e9873b4296e
Roderick Reyes <roderickreyes@Rodericks-MacBook-Pro.local>
2016-11-26 16:14:50 +0100

    updated version with message saving

--- server/server.js
+++ server/server.js
@@ -1,47 +1,90 @@
-const http = require('http');
-const fs = require('fs');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  let url = req.url;
-  switch (url) {
-    case '/' :
-    case '/index.html' :
-      fs.readFile('../src/index.html', (err, data) => {
-        if (err) throw err;
-        // console.log(data);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(data);
-      });
-      break;
-    case '/style.css' :
-      fs.readFile('../src/style.css', (err, data) => {
-        if (err) throw err;
-        // console.log(data);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/css');
-        res.end(data);
-      });
-      break;
-    case '/main.js' :
-      fs.readFile('../src/main.js', (err, data) => {
-        if (err) throw err;
-        // console.log(data);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/js');
-        res.end(data);
-      });
-      break;
-    default:
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Page not found!\n');
-  }
-});
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-server.listen(port, hostname, () => {
+const dbPath = './db.json';
+
+const db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+// JSON.parse(fs.readFileSync(dbPath, {'encoding': 'utf-8'}));
+
+setInterval(function () {
+  fs.writeFile(dbPath, JSON.stringify(db), function () {});
+}, 5000);
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+    // console.log('full request object: ',request);
+
+    var filePath = '../src' + request.url;
+    if (request.url == '/')
+        filePath = '../src/index.html';
+
+    if (request.method === 'POST') {
+      // General body parser on POST requests
+      const body = [];
+      request.on('data', function(chunk) {
+        body.push(chunk);
+      }).on('end', function() {
+        request.body = JSON.parse(Buffer.concat(body).toString());
+      });
+    }
+
+    if (request.url === '/messages' && request.method === 'POST') {
+      request.on('end', function () {
+        console.log(db);
+        db.msgs.push(request.body);
+        response.end();
+      });
+    } else if (request.url === '/messages' && request.method === 'GET') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(db.msgs));
+    } else {
+
+      var extname = String(path.extname(filePath)).toLowerCase();
+      var contentType = 'text/html';
+      var mimeTypes = {
+          '.html': 'text/html',
+          '.js': 'text/javascript',
+          '.css': 'text/css',
+          '.json': 'application/json',
+          '.png': 'image/png',
+          '.jpg': 'image/jpg',
+          '.gif': 'image/gif',
+          '.wav': 'audio/wav',
+          '.mp4': 'video/mp4',
+          '.woff': 'application/font-woff',
+          '.ttf': 'application/font-ttf',
+          '.eot': 'application/vnd.ms-fontobject',
+          '.otf': 'application/font-otf',
+          '.svg': 'application/image/svg+xml'
+      };
+
+      contentType = mimeTypes[extname]; //|| 'application/octect-stream';
+
+      fs.readFile(filePath, function(error, content) {
+          if (error) {
+              if(error.code == 'ENOENT'){
+                  fs.readFile('../src/404.html', function(error, content) {
+                      response.writeHead(200, { 'Content-Type': contentType });
+                      response.end(content, 'utf-8');
+                  });
+              }
+              else {
+                  response.writeHead(500);
+                  response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                  response.end();
+              }
+          }
+          else {
+              response.writeHead(200, { 'Content-Type': contentType });
+              response.end(content, 'utf-8');
+          }
+      });
+    }
+
+}).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- src/main.js
+++ src/main.js
@@ -1,18 +1,85 @@
 function parseQuote (response) {
-  $('.chatWindow').append('<div class="response">'+ response.quoteText + '</div>');
+  renderMsg(response.quoteText, 'response');
+  saveMessage('user2', response.quoteText, Date.now());
+}
+
+function getMessages (data) {
+  $.ajax({
+    url: '/messages',
+    method: 'GET',
+    // data: JSON.stringify(),
+    // dataType: 'json',
+    success: function (db) {
+      db.msgs.forEach(function (msg) {
+        renderMsg(msg);
+      });
+    },
+    error: function(err) {
+      console.log(err);
+      return;
+    },
+  });
+}
+
+function renderMsgJSON (msg, className) {
+  const $msgHtml = $('<div class="msg">').html(msg.content);
+  if (className) $msgHtml.addClass(className);
+  else if (msg.userId === 'user1') $msgHtml.addClass('userMsg');
+  $('.chatWindow').append($msgHtml);
+}
+
+function renderMsg (msg, className) {
+  const $msgHtml = $('<div class="msg">').html(msg);
+  if (className) $msgHtml.addClass(className);
+  $('.chatWindow').append($msgHtml);
+}
+
+function saveMessage (userId, content, timeStamp) {
+  $.ajax({
+    url: '/messages',
+    method: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      userId,
+      content,
+      timeStamp
+    }),
+    success: function (data) {
+      console.log('data saved:', data);
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
 }
 
 function getQuote() {
   $.ajax({
     url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote',
     type: 'GET',
-    dataType: 'jsonp'
+    dataType: 'jsonp',
+    success: function (data) {
+      // saveMessage(data.quoteText);
+    }
   });
 }
 
-$(document).ready(function() {
-  $('#target').submit(function(event) {
+$(document).ready(function () {
+  $('#target').submit(function (event) {
     event.preventDefault();
+    let userText = $('#textbox').val();
+    if (userText) renderMsg(userText, 'userMsg');
+    $('#textbox').val('');
+    saveMessage('user1', userText, Date.now());
     getQuote();
+    // chatDiv.scrollTop = chatDiv.scrollHeight;
+    // let height = chatDiv.scrollHeight;
+    // chatDiv.scrollTop = chatDiv.height();
+    // let chatDiv = document.getElementsByClassName('chatWindow')[0];
+    let chatDiv = $('.chatWindow');
+    chatDiv.clientHeight = chatDiv.scrollHeight;
+    // chatDiv.scrollTop = $(".msg").last().offset().top;//chatDiv.scrollHeight;
+    // console.log('chatDiv height',chatDiv.height(),'chatDiv.scrollTop',chatDiv.scrollTop, 'chatDiv.scrollHeight', chatDiv.scrollHeight);
   });
 });
